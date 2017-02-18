f2de6fd32f21438db56731a9af97201abb30f93e
cavanx <lichengdong@lichengdongdeMacBook-Pro.local>
2017-02-18 22:40:24 +0800

    first

---
+++ im/im/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  im
+//
+//  Created by 历成栋 on 2017/2/18.
+//  Copyright © 2017年 cavan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ im/im/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  im
+//
+//  Created by 历成栋 on 2017/2/18.
+//  Copyright © 2017年 cavan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"askdakld");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
